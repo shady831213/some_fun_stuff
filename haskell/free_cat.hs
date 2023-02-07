5eaa91a606d92a22cadbcb1c074f888bbe34a73e
shady831213 <shady831213@126.com>
2023-02-07 17:16:48 +0800

    scatching haskell challenge

--- haskell/free_cat.hs
+++ haskell/free_cat.hs
@@ -2,9 +2,19 @@
 module FreeCat where
 import Control.Category
 newtype Node = Node String
+
 data SingleNodeCat a b = SingleNodeCat Node
 singleNodeCatObj :: SingleNodeCat a b
 singleNodeCatObj = SingleNodeCat(Node "")
+
 instance Category SingleNodeCat where
     id = singleNodeCatObj
-    (.) _ _ = singleNodeCatObj
+    (.) _ _ = undefined
+
+data SingleNodeSingleArrowCat a b = SingleNodeSingleArrowCat Node
+singleNodeSingleArrowCatObj :: SingleNodeSingleArrowCat a b
+singleNodeSingleArrowCatObj = SingleNodeSingleArrowCat(Node "")
+
+instance Category SingleNodeSingleArrowCat where
+    id = singleNodeSingleArrowCatObj
+    (.) _ _ = singleNodeSingleArrowCatObj
